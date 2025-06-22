dd7d583a3e204a1e2a1d443220d1ce7662812607
Eugene <aktemirov@yandex.ru>
2025-06-22 14:52:12 +0300

    edit review1 permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,7 +7,7 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and (request.user.role == 'admin' or request.user.is_superuser)
+            and request.user.is_admin
         )
 
 
@@ -28,9 +28,8 @@ class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            or request.user.role == 'moderator'
-            or request.user.role == 'admin'
-            or request.user.is_superuser
+            or request.user.is_moderator
+            or request.user.is_admin
         )
 
 
