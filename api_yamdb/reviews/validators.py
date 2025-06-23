f0ea4b60e16e5ba6985dbc69361b0aa942e605cc
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2025-06-23 06:05:58 +0500

    migrate

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,10 +1,6 @@
 import re
 
-from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from rest_framework import serializers
-
-from .constants import MAX_FIRST_LAST_NAME_LENGTH
 
 
 def validate_username_format(value):
@@ -21,35 +17,5 @@ def validate_username_format(value):
     if value.lower() == 'me':
         raise ValidationError(
             'Использовать имя "me" в качестве username запрещено'
-        )
-    return value
-
-
-def validate_user_exists(username, email):
-    """Проверяет существование пользователя и уникальность полей."""
-    user_model = get_user_model()
-    user = user_model.objects.filter(username=username).first()
-    if user:
-        if user.email != email:
-            raise serializers.ValidationError(
-                'Пользователь с таким username уже существует с другим email'
-            )
-        return user
-    else:
-        if user_model.objects.filter(email=email).exists():
-            raise serializers.ValidationError(
-                'Пользователь с таким email уже существует'
-            )
-        return None
-
-
-def validate_name_length(value, field_name):
-    """Проверяет, что длина имени/фамилии не превышает допустимое значение."""
-    if not value:
-        return value
-    if len(value) > MAX_FIRST_LAST_NAME_LENGTH:
-        raise serializers.ValidationError(
-            f'Длина {field_name} не должна превышать'
-            ' {MAX_FIRST_LAST_NAME_LENGTH} символов'
         )
     return value
